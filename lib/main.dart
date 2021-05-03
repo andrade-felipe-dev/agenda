c3a9f820a34ce3b10b2acfc014d73181b4c35cf9
andrade-felipe-dev <andradefelipedev@gmail.com>
2021-05-03 14:05:01 -0300

    Commit
    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:agenda/ui/contact_page.dart';
+import 'package:agenda/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
+
